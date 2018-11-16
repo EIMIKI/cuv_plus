082a83aea240c45cbb98dda522bc04316e2770e5
EIMIKI <kou.103.kan@gmail.com>
2018-11-16 17:57:03 +0900

    トップページ(更新一覧ページ)作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @comics=Comic.where(updated_at:Time.zone.now.all_day)
+    @old_comics=Comic.where.not(updated_at:Time.zone.now.all_day)
   end
 end
