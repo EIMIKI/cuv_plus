c6812515db8931c6985a7498025dc60ef5a02404
EIMIKI <kou.103.kan@gmail.com>
2018-11-16 13:46:36 +0900

    作品用のmodelを作成

---
+++ db/migrate/20181116043307_create_comics.rb
@@ -0,0 +1,12 @@
+class CreateComics < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comics do |t|
+      t.string :title
+      t.string :url
+      t.string :thum_url
+      t.integer :site
+
+      t.timestamps
+    end
+  end
+end
