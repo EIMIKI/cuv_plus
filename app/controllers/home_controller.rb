dccd4b4fa70f5728ec22049779b75779785efb1e
EIMIKI <kou.103.kan@gmail.com>
2018-11-23 20:29:59 +0900

    作品の表示順を変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @comics=Comic.where(updated_at:Time.zone.now.all_day)
-    @old_comics=Comic.where.not(updated_at:Time.zone.now.all_day)
+    @comics=Comic.where(updated_at:Time.zone.now.all_day).shuffle
+    @old_comics=Comic.where.not(updated_at:Time.zone.now.all_day).order("updated_at DESC")
   end
 end
