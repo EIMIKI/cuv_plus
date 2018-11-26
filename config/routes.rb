ab10aca1fbcb38f106d7fba5abd449202ed1f8fa
EIMIKI <kou.103.kan@gmail.com>
2018-11-26 22:03:23 +0900

    設定ページで選択した作品がサーバのログに表示されるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/setting' => 'setting#index'
   get '/' => 'home#index'
+  post '/setting/post' => 'setting#post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
