a9953d7044fcd526039d23730a6c9715d58edc9d
EIMIKI <kou.103.kan@gmail.com>
2018-11-29 16:09:10 +0900

    チェック状態がページに反映されるように

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -3,6 +3,14 @@ class SettingController < ApplicationController
     @us_comics=Comic.where(site_id:Site.find_by(name:'urasunday')[:id])
     @sw_comics=Comic.where(site_id:Site.find_by(name:'sundaywebry')[:id])
     @ys_comics=Comic.where(site_id:Site.find_by(name:'yawaraka')[:id])
+
+
+    checked_comics=Comic.includes(:users).where('users.id'=>session[:user])
+    @checked_comics_id=[]
+    checked_comics.each do |comic|
+      @checked_comics_id.push(comic.id)
+    end
+
   end
 
   def post
