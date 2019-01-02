03b279571fcf949c2af88d4f9ec9289a46eb6d12
EIMIKI <kou.103.kan@gmail.com>
2019-01-02 20:13:58 +0900

    アクセスのたびに既存のブラウザ情報が複製されていたので修正

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,12 +1,12 @@
 class DevicesController < ApplicationController
   def create
     user = User.find(session[:user])
-    
-    device = Device.find_by(user_id:user)
+
+    device = Device.find_by(user_id:user.id)
     if !device
       device=Device.new
     end
-    device = Device.new(endpoint:params[:subscription][:endpoint],
+    device = Device.update(endpoint:params[:subscription][:endpoint],
                         p256dh:params[:subscription][:keys][:p256dh],
                         auth:params[:subscription][:keys][:auth],
                         user_id:user.id)
