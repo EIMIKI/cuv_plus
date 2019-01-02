f9e66e49016d6d477769ab2deeb1c7c799cffd57
EIMIKI <kou.103.kan@gmail.com>
2019-01-02 21:07:41 +0900

    新規にブラウザ情報が登録できなくなっていたので修正

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,11 +2,8 @@ class DevicesController < ApplicationController
   def create
     user = User.find(session[:user])
 
-    device = Device.find_by(user_id:user.id)
-    if !device
-      device=Device.new
-    end
-    device = Device.update(endpoint:params[:subscription][:endpoint],
+    device = Device.find_or_initialize_by(user_id:user.id)
+    device.update(endpoint:params[:subscription][:endpoint],
                         p256dh:params[:subscription][:keys][:p256dh],
                         auth:params[:subscription][:keys][:auth],
                         user_id:user.id)
