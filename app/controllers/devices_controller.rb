7f23c7c5a3e0bbcb993cc321190d1386437d3564
EIMIKI <kou.103.kan@gmail.com>
2019-01-02 19:02:20 +0900

    ブラウザの情報をユーザをキーとして探すように

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,14 +1,16 @@
 class DevicesController < ApplicationController
   def create
-    device = Device.find_by(endpoint:params[:subscription][:endpoint])
+    user = User.find(session[:user])
+    
+    device = Device.find_by(user_id:user)
     if !device
-      user = User.find(session[:user])
-      device = Device.new(endpoint:params[:subscription][:endpoint],
-                          p256dh:params[:subscription][:keys][:p256dh],
-                          auth:params[:subscription][:keys][:auth],
-                          user_id:user.id)
-
-      device.save
+      device=Device.new
     end
+    device = Device.new(endpoint:params[:subscription][:endpoint],
+                        p256dh:params[:subscription][:keys][:p256dh],
+                        auth:params[:subscription][:keys][:auth],
+                        user_id:user.id)
+
+    device.save
   end
 end
